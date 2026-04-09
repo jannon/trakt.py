de916cfde992266fbd27b8027df688b17534e510
Jannon Frank <theoriginal@gmail.com>
2026-04-08 19:32:52 -0700

    Add support for dropped shows

--- trakt/interfaces/users/hidden.py
+++ trakt/interfaces/users/hidden.py
@@ -1,34 +1,43 @@
+import requests
 
 from trakt.core.helpers import dictfilter
 from trakt.core.pagination import PaginationIterator
 from trakt.interfaces.base import Interface, authenticated
 from trakt.mapper.hidden import HiddenItemMapper
 
-import requests
-
 
 class UsersHiddenInterface(Interface):
-    path = 'users/hidden/*'
-    _valid_sections = ['calendar', 'progress_watched', 'progress_collected',
-                       'recommendations', 'comments']
-    _valid_types = ['movie', 'show', 'season', 'user']
+    path = "users/hidden/*"
+    _valid_sections = [
+        "calendar",
+        "dropped",
+        "progress_watched",
+        "progress_collected",
+        "recommendations",
+        "comments",
+    ]
+    _valid_types = ["movie", "show", "season", "user"]
 
     @authenticated
     def get(self, section, type=None, page=None, per_page=None, **kwargs):
         if section not in self._valid_sections:
-            raise ValueError(f'Unknown section specified: {section}')
+            raise ValueError(f"Unknown section specified: {section}")
 
         if type and type not in self._valid_types:
-            raise ValueError(f'Unknown type specified: {type}')
+            raise ValueError(f"Unknown type specified: {type}")
 
-        response = self.http.get(f'/users/hidden/{section}',
-                                 query={'type': type, 'page': page, 'limit': per_page},
-                                 **dictfilter(kwargs, pop=['authenticated', 'pagination', 'validate_token']))
+        response = self.http.get(
+            f"/users/hidden/{section}",
+            query={"type": type, "page": page, "limit": per_page},
+            **dictfilter(kwargs, pop=["authenticated", "pagination", "validate_token"]),
+        )
 
         items = self.get_data(response, **kwargs)
 
         if isinstance(items, PaginationIterator):
-            return items.with_mapper(lambda items: HiddenItemMapper.hidden_items(self.client, items))
+            return items.with_mapper(
+                lambda items: HiddenItemMapper.hidden_items(self.client, items)
+            )
 
         if isinstance(items, requests.Response):
             return items
@@ -38,19 +47,25 @@ class UsersHiddenInterface(Interface):
     @authenticated
     def add(self, section, items, **kwargs):
         if section not in self._valid_sections:
-            raise ValueError(f'Unknown section specified: {section}')
+            raise ValueError(f"Unknown section specified: {section}")
 
-        response = self.http.post(f'/users/hidden/{section}', data=items,
-                                  **dictfilter(kwargs, pop=['authenticated', 'validate_token']))
+        response = self.http.post(
+            f"/users/hidden/{section}",
+            data=items,
+            **dictfilter(kwargs, pop=["authenticated", "validate_token"]),
+        )
 
         return self.get_data(response, **kwargs)
 
     @authenticated
     def remove(self, section, items, **kwargs):
         if section not in self._valid_sections:
-            raise ValueError(f'Unknown section specified: {section}')
+            raise ValueError(f"Unknown section specified: {section}")
 
-        response = self.http.post(f'/users/hidden/{section}/remove', data=items,
-                                  **dictfilter(kwargs, pop=['authenticated', 'validate_token']))
+        response = self.http.post(
+            f"/users/hidden/{section}/remove",
+            data=items,
+            **dictfilter(kwargs, pop=["authenticated", "validate_token"]),
+        )
 
         return self.get_data(response, **kwargs)
